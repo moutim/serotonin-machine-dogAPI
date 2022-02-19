fa0246f16aab6bda0e49751ee0bcc856db82bc37
Vitor <moutimg@gmail.com>
2022-02-19 13:39:15 -0300

    Adiciona funcao que transforma string de racas em array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css'
 import { DualRing } from 'react-awesome-spinners';
+import getListBreeds from './services/getListBreeds';
 import fetchAPI from './services/fetchAPI';
 import CardDog from './components/CardDog';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material';
 
 class App extends Component {
   state = {
@@ -31,6 +33,25 @@ class App extends Component {
         <Header />
         <main>
           <h1>Serotonin Machine</h1>
+
+          <FormControl>
+      <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
+        <RadioGroup
+          row
+          aria-labelledby="demo-row-radio-buttons-group-label"
+          name="row-radio-buttons-group"
+        >
+          <FormControlLabel value="female" control={<Radio />} label="Female" />
+          <FormControlLabel value="male" control={<Radio />} label="Male" />
+          <FormControlLabel value="other" control={<Radio />} label="Other" />
+          <FormControlLabel
+            value="disabled"
+            disabled
+            control={<Radio />}
+            label="other"
+          />
+        </RadioGroup>
+      </FormControl>
           {
             loading ? <div className="loading"><DualRing color="#ff9500" /></div>  : (
               <CardDog url={ message } dogName={ breedFormated } />
